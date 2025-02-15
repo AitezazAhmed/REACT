6582cc8378c83914381d8f11ac5f601a8d773107
AitezazAhmed <stoneghold4103@gmail.com>
2025-02-15 13:25:07 +0500

    Nav.jsx

---
+++ 06_components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+   <nav className='flex  items-center gap-20'>
+    <h1>Home</h1>
+    <h1>Contacts</h1>
+    <h1>Services</h1>
+    <h1>Your Account</h1>
+   </nav>
+  )
+}
+
+export default Nav
