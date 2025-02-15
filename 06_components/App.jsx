1a8b55b38d1bd560d5c1f19becb2d15c01d23820
AitezazAhmed <stoneghold4103@gmail.com>
2025-02-15 13:25:15 +0500

    App.jsx

---
+++ 06_components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Components/Header'
+import Nav from './Components/Nav'
+const App = () => {
+  return (
+  <>
+ <Header />
+  </>
+
+  )
+}
+
+export default App
